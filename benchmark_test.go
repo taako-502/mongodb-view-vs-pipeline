abd081a981d8887a974d2a2e87c687f4060e00c3
taako-502 <yamamoto.takao502@gmail.com>
2025-02-24 00:10:00 +0900

    refactor: ベンチマークの書き方に変更

---
+++ benchmark_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/taako-502/mongodb-view-vs-pipeline/pkg/service"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+const (
+	mongoURI       = "mongodb://localhost:27017"
+	databaseName   = "testdb"
+	collectionName = "testcollection"
+	viewName       = "testview"
+)
+
+func BenchmarkMongoDB(b *testing.B) {
+	client, err := mongo.Connect(options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		b.Fatalf("MongoDB connection error: %v", err)
+	}
+	defer client.Disconnect(context.TODO())
+
+	db := client.Database(databaseName)
+	collection := db.Collection(collectionName)
+
+	s := service.NewService(collection, viewName, collectionName)
+	if err := s.CreateView(db); err != nil {
+		b.Fatalf("Failed to create view: %v", err)
+	}
+	if err := s.DropSampleData(); err != nil {
+		b.Fatalf("Failed to drop sample data: %v", err)
+	}
+
+	test := []int64{10000, 100000, 1000000, 10000000}
+	for _, num := range test {
+		b.Run(fmt.Sprintf("Documents_%d", num), func(b *testing.B) {
+			if err := s.InsertSampleData(num); err != nil {
+				b.Fatalf("Failed to insert sample data: %v", err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				_, err := s.BenchmarkViewFind(db)
+				if err != nil {
+					b.Fatalf("Failed to find view: %v", err)
+				}
+				_, err = s.BenchmarkAggregationFind()
+				if err != nil {
+					b.Fatalf("Failed to find aggregation: %v", err)
+				}
+			}
+			b.StopTimer()
+			if err := s.DropSampleData(); err != nil {
+				b.Fatalf("Failed to drop sample data: %v", err)
+			}
+		})
+	}
+}
